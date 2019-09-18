724fc82b45e045159c29facb6a4ccd64b1b964c0
omer-friedman <34510256+omer-friedman@users.noreply.github.com>
2019-09-18 13:35:38 +0300

    start make authetication

--- app/src/main/java/com/example/bishuliko/MainActivity.java
+++ app/src/main/java/com/example/bishuliko/MainActivity.java
@@ -6,18 +6,42 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
-//    private FirebaseAuth mAuth;
+
+    private static final int MY_REQUEST_CODE = 7117;
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mAuth = FirebaseAuth.getInstance();
+        providers = new Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(), //Email Builder
+                new AuthUI.IdpConfig.PhoneBuilder().build(), //Phone Builder
+                new AuthUI.IdpConfig.FacebookBuilder().build(), //Facebook Builder
+                new AuthUI.IdpConfig.GoogleBuilder().build() //Google Builder
+        );
+        
+        showSignInOptions();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                .build(), MY_REQUEST_CODE
+        );
+
     }
 
     public void go_to_menu(View view) {
@@ -25,4 +49,7 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.startActivity(myIntent);
     }
 
+    public void register(View view){
+
+    }
 }
--- app/src/main/java/com/example/bishuliko/SearchCategory.java
+++ app/src/main/java/com/example/bishuliko/SearchCategory.java
@@ -1,6 +1,5 @@
 package com.example.bishuliko;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
